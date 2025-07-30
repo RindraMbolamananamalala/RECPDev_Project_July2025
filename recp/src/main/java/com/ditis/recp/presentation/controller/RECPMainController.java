45b519016b1ca7f1d3f0e6208717435a9614b29a
RindraMbolamananamalala <rindraibi@gmail.com>
2025-07-30 17:48:22 +0200

    RECP - HMI

--- recp/src/main/java/com/ditis/recp/presentation/controller/RECPMainController.java
+++ recp/src/main/java/com/ditis/recp/presentation/controller/RECPMainController.java
@@ -1,14 +1,16 @@
 package com.ditis.recp.presentation.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class RECPMainController{
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from the RECP!";
+	@RequestMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index.html");
 	}
 
 }

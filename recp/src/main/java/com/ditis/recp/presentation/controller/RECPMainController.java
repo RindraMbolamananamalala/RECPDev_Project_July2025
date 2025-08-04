ac1dc9960ae544a7963e212bb4cf0628a3a11162
RindraMbolamananamalala <rindraibi@gmail.com>
2025-08-04 16:26:36 +0200

    Implementation: Information on a found pattern displayed on the HMI (main page)

--- recp/src/main/java/com/ditis/recp/presentation/controller/RECPMainController.java
+++ recp/src/main/java/com/ditis/recp/presentation/controller/RECPMainController.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -43,23 +45,34 @@ public class RECPMainController{
 		}
 		return mainPageMNV;
 	}
-
+	
+	/**
+	 * Displaying the main page
+	 * @return
+	 */
 	@GetMapping("/")
-	public ModelAndView mainPage(
-			@RequestParam(name="title"
-								, required=false
-								, defaultValue="Référentiel d'Expertise, de Connaissances et de Pratiques"
-							) String title
-		, Model  model) {
-		System.out.println("HERERERE=");
-		model.addAttribute("title", title);
-		// TEST READ DB
-		List<PatternEntity> patternsRead = patternAS.findPatterns("MVC Name");
-		for (PatternEntity pattern : patternsRead) {
-			System.out.println("Pattern now as=" + pattern.toString());
-		}
-		System.out.println("NOMBRE MVC now AS=" + patternsRead.size());
-		// TEST READ DB
+	public ModelAndView mainPage() {
 		return this.getMainPageMNV();
+	}
+	
+	/**
+	 * Searching the list of patterns that have the same name as that specified by the user within the 'Pattern's name" input text 
+	 * @param inputPatternName The name of the patterns to search
+	 * @param model The current model being used by the RECP Application
+	 * @return The actualized (with patterns' data retrieved from the RECP's patterns DB) version of the main page
+	 */
+	@PostMapping(value="/search_patterns")
+	public ModelAndView researchPattern(String inputPatternName, Model model) {
+		String patternNameInput = inputPatternName;
+		// searching the patterns
+		List<PatternEntity> patternsRead = patternAS.findPatterns(patternNameInput);
+		// (SO FAR), only keeping the first one from the result obtained previously 
+		PatternEntity patternRead = patternsRead.get(0);
+		// Updating the HMI (main page) with the data obtained from the RECP's Patterns DB
+	    model.addAttribute("patternName", patternRead.getName());
+	    model.addAttribute("patternProblemToSolve", patternRead.getProblem());
+	    
+	    //displaying the actualized version of the Main HMI
+	    return this.getMainPageMNV();
 	}
 }

5dd97b010152a2afad89a1cdf62c299571541b34
RindraMbolamananamalala <rindraibi@gmail.com>
2026-09-10 22:46:13 +0200

    Making it possible to call internal REST API of the RECP Tool within Microsoft Azure

--- recp/src/main/java/com/ditis/recp/business/apiservice/internalapiservice/impl/RECPSCRUDAPIServiceImpl.java
+++ recp/src/main/java/com/ditis/recp/business/apiservice/internalapiservice/impl/RECPSCRUDAPIServiceImpl.java
@@ -20,7 +20,7 @@ public class RECPSCRUDAPIServiceImpl implements RECPSCRUDAPIServiceIntf{
 				  .build();
 		MediaType mediaType = MediaType.parse("text/plain");
 		Request request = new Request.Builder()
-				  .url("http://0.0.0.0:8090/search_patterns?typeOfPatternResearch=patternResearchByName&inputPatternInformation=" + patternName)
+				  .url("http://127.0.0.1:8090/search_patterns?typeOfPatternResearch=patternResearchByName&inputPatternInformation=" + patternName)
 				  .method("GET", null)
 				  .build();
 		try {
